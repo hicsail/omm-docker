f49e81900df09277d02ddd24a01b08ccffdcd13d
Akosah <emmanuel.s.akosah.19@gmail.com>
2024-10-09 19:01:03 -0400

    track page focus and unfocus

--- app/common/gtag_setup.php
+++ app/common/gtag_setup.php
@@ -33,7 +33,53 @@ $track_ga = isset($_SESSION['track_ga']) ? $_SESSION['track_ga'] : (isset($_COOK
             });
         }
 
+        console.log('window.focusBlurListenersAdded', window.focusBlurListenersAdded);
 
+        if (!window.focusBlurListenersAdded) {
+            window.focusBlurListenersAdded = true;
+
+            var pageTitleStr = "<?php echo isset($pageTitle) ? $pageTitle : 'Default Title'; ?>";
+            var unfocusStartTime = null;
+            let isNavigatingAway = false;
+
+            // Function to handle page focus
+            function handleFocus() {
+                if (unfocusStartTime) {
+                    gtag("event", "page_focused", {
+                        subid: "<?php echo $subid; ?>",
+                        page: pageTitleStr,
+                        time_spent_unfocused: Math.floor((Date.now() - unfocusStartTime) / 1000),
+                        timestamp: Date.now(),
+                        event_callback: function() {
+                            console.log("page_focused event sent to Google Analytics");
+                        },
+                    });
+                    unfocusStartTime = null;
+                }
+            }
+
+            function handleBlurOrVisibilityChange() {
+                if (document.visibilityState === "hidden" && !isNavigatingAway) {
+                    unfocusStartTime = Date.now();
+                    console.log("Page is unfocused");
+                    gtag("event", "page_unfocused", {
+                        subid: "<?php echo $subid; ?>",
+                        page: pageTitleStr,
+                        timestamp: Date.now(),
+                        event_callback: function() {
+                            console.log("page_unfocused event sent to Google Analytics");
+                        },
+                    });
+                }
+            }
+
+            window.addEventListener("beforeunload", function() {
+                isNavigatingAway = true;
+            });
+
+            window.addEventListener("focus", handleFocus);
+            document.addEventListener("visibilitychange", handleBlurOrVisibilityChange);
+        }
     } else {
         console.log('Google Analytics tracking is disabled');
     }
