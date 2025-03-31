36364b52bf7cd4f092a21838a25e3a34d655d9b6
Zoe <19900057+vpsx@users.noreply.github.com>
2025-03-31 11:18:19 -0400

    Add hidden spans to differentiate s

--- app/statements/bank_statement_content.php
+++ app/statements/bank_statement_content.php
@@ -94,12 +94,12 @@
             <div style="position:absolute;left:329.63px;top:494.90px" class="cls_006"><span class="cls_006" onclick="stopTimer(this),setColor(event)" ; data-count="1">payment each period, you will pay more in interest and it will</span></div>
             <div onclick="stopTimer(this),setColor(event)" ; data-count="0">
                 <div style="position:absolute;left:26.00px;top:496.65px" class="cls_013"><span class="cls_013">Total Cash Advance Line</span></div>
-                <div style="position:absolute;left:266.35px;top:497.15px" class="cls_002"><span class="cls_002">$900.00</span></div>
+                <div style="position:absolute;left:266.35px;top:497.15px" class="cls_002"><span class="cls_002">$900.00<span hidden=true>(total cash advance line)</span></span></div>
             </div>
             <div style="position:absolute;left:329.63px;top:506.90px" class="cls_006"><span class="cls_006" onclick="stopTimer(this),setColor(event)" ; data-count="1">take you longer to pay off your balance. For example:</span></div>
             <div class="possible_answer" id="2" onclick="stopTimer(this),setColor(event)" ; data-count="0">
                 <div style="position:absolute;left:26.00px;top:520.67px" class="cls_013"><span class="cls_013">Available for Cash</span></div>
-                <div style="position:absolute;left:266.35px;top:521.17px" class="cls_002"><span class="cls_002">$900.00</span></div>
+                <div style="position:absolute;left:266.35px;top:521.17px" class="cls_002"><span class="cls_002">$900.00<span hidden=true>(available for cash)</span></span></div>
             </div>
             <div style="position:absolute;left:348.65px;top:531.42px" class="cls_002"><span class="cls_002" onclick="stopTimer(this),setColor(event)" ; data-count="1">If you make no</span></div>
             <div style="position:absolute;left:446.18px;top:531.42px" class="cls_002"><span class="cls_002" onclick="stopTimer(this),setColor(event)" ; data-count="1">You will pay off</span></div>
